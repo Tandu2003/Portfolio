61529be2f18cd3f172efbb21ff6d2a22e7b49fab
Nguyễn Tấn Dự <92675947+Tandu2003@users.noreply.github.com>
2023-03-27 01:00:39 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// toggle icon navbar
+Let menuicon = document.querySelector('#menu-icon');
+Let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuicon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// scroll sections active link
+Let sections = document.querySelectorAll('section');
+Let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        Let top = window.scrollY;
+        Let offset = sec.offsetTop - 150;
+        Let height = sec.offsetHeight;
+        Let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    // sticky navbar
+    Let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
